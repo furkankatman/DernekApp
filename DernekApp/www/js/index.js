fdda091aa8e4177cfe070e4ac25dc2df86899500
furkankatman <furkan.katman@gmail.com>
2019-03-19 19:42:44 +0300

    latest stable version.

--- DernekApp/www/js/index.js
+++ DernekApp/www/js/index.js
@@ -28,10 +28,66 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         
-        var app=angular.module("DernekApp",['ngMaterial', 'ngMessages']);
+        var app=angular.module("DernekApp",['ngMaterial', 'ngMessages','ui.router']);
 
-        app.controller("MainController",function($scope){
+        app.controller("MainController",function($scope, $timeout, $mdSidenav, $log){
+            $scope.toggleLeft = buildDelayedToggler('left');
+            $scope.toggleRight = buildToggler('right');
+            $scope.isOpenRight = function(){
+              return $mdSidenav('right').isOpen();
+            };
+             /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+        var timer;
+  
+        return function debounced() {
+          var context = $scope,
+              args = Array.prototype.slice.call(arguments);
+          $timeout.cancel(timer);
+          timer = $timeout(function() {
+            timer = undefined;
+            func.apply(context, args);
+          }, wait || 10);
+        };
+      }
+  
+      /**
+       * Build handler to open/close a SideNav; when animation finishes
+       * report completion in console
+       */
+      function buildDelayedToggler(navID) {
+        return debounce(function() {
+          // Component lookup should always be available since we are not using `ng-if`
+          $mdSidenav(navID)
+            .toggle()
+            .then(function () {
+              $log.debug("toggle " + navID + " is done");
+            });
+        }, 200);
+      }
+  
+      function buildToggler(navID) {
+        return function() {
+          // Component lookup should always be available since we are not using `ng-if`
+          $mdSidenav(navID)
+            .toggle()
+            .then(function () {
+              $log.debug("toggle " + navID + " is done");
+            });
+        };
+      }
 
+      $scope.close = function () {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav('left').close()
+          .then(function () {
+            $log.debug("close LEFT is done");
+          });
+  
+      };
             $scope.Greeting="Merhaba Sayın Üye :)";
             $scope.dernek={};
 
@@ -48,7 +104,32 @@ var app = {
             angular.bootstrap(document, ['DernekApp']);
           });
 
+          app.config(function($stateProvider,$urlRouterProvider) {
+            var homeState = {
+              name: 'home',
+              url: '/home',
+              templateUrl: '../Templates/Home.html',
+              controller:function($scope){
+                $scope.Greeting="Selam Home";
+                $scope.today=new Date();
 
+
+              }
+            }
+          
+            var aboutState = {
+              name: 'about',
+              url: '/about',
+              templateUrl: '../Templates/About.html',
+              controller:function($scope){
+                $scope.Greeting="Selam About";
+            }
+            }
+          
+            $stateProvider.state(homeState);
+            $stateProvider.state(aboutState);
+            $urlRouterProvider.otherwise('/home');
+          });
 
 
     }
